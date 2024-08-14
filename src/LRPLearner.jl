3c91a68fa5a9a07c1ef2dab16b2299f6fd4bf39a
Henri Kauhanen <nenahuak@gmail.com>
2024-08-14 14:43:14 +0200

    allow learning rate vectors, i.e. different learning rates for different actions

--- src/LRPLearner.jl
+++ src/LRPLearner.jl
@@ -1,10 +1,15 @@
 """
-    LRPLearner(n::Int, a::Float64, b::Float64, W::Vector{Float64}, R::Vector{Matrix{Float64}}, P::Vector{Matrix{Float64}}) <: AbstractLinearLearner
+    LRPLearner(n::Int,
+               a::Vector{Float64},
+               b::Vector{Float64}, 
+               W::Vector{Float64},
+               R::Vector{Matrix{Float64}}, 
+               P::Vector{Matrix{Float64}}) <: AbstractLinearLearner
 
 A linear reward-penalty learner.
 
-A general linear reward-penalty learner with `n` actions, learning rate `a`
-for rewards, learning rate `b` for punishments, action probability
+A general linear reward-penalty learner with `n` actions, learning rates `a`
+for rewards, learning rates `b` for punishments, action probability
 vector `W`, and vectors of reward and penalty operators `R` and `P`.
 
 # References
@@ -13,8 +18,8 @@ Bush and Mosteller FIXME
 """
 mutable struct LRPLearner <: AbstractLinearLearner
   n::Int
-  a::Float64
-  b::Float64
+  a::Vector{Float64}
+  b::Vector{Float64}
   W::Vector{Float64}
   R::Vector{Matrix{Float64}}
   P::Vector{Matrix{Float64}}
@@ -22,7 +27,7 @@ end
 
 
 # custom pretty-printing for LRPLearners
-Base.show(io::IO, z::LRPLearner) = print(io, "Linear reward-penalty learner (LRPLearner) with ", z.n, " actions\n\nLearning rate for reward: ", z.a, "\nLearning rate for penalty: ", z.b, "\n\nCurrent action probability vector: ", z.W)
+Base.show(io::IO, z::LRPLearner) = print(io, "Linear reward-penalty learner (LRPLearner) with ", z.n, " actions\n\nLearning rates for reward: ", z.a, "\nLearning rates for penalty: ", z.b, "\n\nCurrent action probability vector: ", z.W)
 
 
 """
@@ -49,38 +54,26 @@ LRPLearner(n::Int, a::Float64, b::Float64) = LRPLearner(n, ones(n) ./ n, a, b)
 Create an `LRPLearner` with `n` actions, initial action probability vector `W`,
 and learning rates `a` and `b`.
 """
-function LRPLearner(n::Int, W::Vector{Float64}, a::Float64, b::Float64)
+LRPLearner(n::Int, W::Vector{Float64}, a::Float64, b::Float64) = LRPLearner(n, W, repeat([a], outer=n), repeat([b], outer=n))
+
+
+"""
+    LRPLearner(n::Int, W::Vector{Float64}, a::Vector{Float64}, b::Vector{Float64})
+
+Create an `LRPLearner` with `n` actions, initial action probability vector `W`,
+and learning rate vectors `a` and `b`.
+"""
+function LRPLearner(n::Int, W::Vector{Float64}, a::Vector{Float64}, b::Vector{Float64})
     R = Vector{Matrix{Float64}}(undef, 0)
     P = Vector{Matrix{Float64}}(undef, 0)
     
     # construct reward and penalty matrices
     for i in 1:n
-        push!(R, (1 - a) * LinearAlgebra.I + a * matrixunit(n, i) * ones(n, n))
-        push!(P, (1 - b) * LinearAlgebra.I + (b/(n - 1)) * (ones(n, n) - matrixunit(n, i)* ones(n, n)))
+        push!(R, (1 - a[i]) * LinearAlgebra.I + a[i] * matrixunit(n, i) * ones(n, n))
+        push!(P, (1 - b[i]) * LinearAlgebra.I + (b[i]/(n - 1)) * (ones(n, n) - matrixunit(n, i)* ones(n, n)))
     end
     
     return LRPLearner(n, a, b, W, R, P)
 end
 
 
-#=
-"""
-    reward!(x::LRPLearner, i::Int)
-
-Reward the `i`th action of an `LRPLearner`.
-"""
-function reward!(x::LRPLearner, i::Int)
-  x.W = ((1 - x.gamma) * LinearAlgebra.I + x.gamma * matrixunit(x.dim, i) * ones(x.dim, x.dim)) * x.W
-end
-
-
-"""
-    punish!(x::LRPLearner, i::Int)
-
-Punish the `i`th action of an `LRPLearner`.
-"""
-function punish!(x::LRPLearner, i::Int)
-  x.W = ((1 - x.gamma) * LinearAlgebra.I + (x.gamma/(x.dim - 1)) * (ones(x.dim, x.dim) - matrixunit(x.dim, i)*ones(x.dim, x.dim))) * x.W
-end
-
-=#
